ba2cca0cbb7330422084129eb2a37cbc533ff045
Alexandr <87748202+shadowHD-bit@users.noreply.github.com>
2022-07-05 15:16:39 +0700

    create auth page and start create auth script

--- client/page/AuthPage/AuthPage.php
+++ client/page/AuthPage/AuthPage.php
@@ -1,10 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Авторизация</title>
+    <link rel="stylesheet" href="./AuthPage.css">
 </head>
-<body>
 
+<body>
+    <div class="auth_content">
+        <div class="auth_card">
+            <div class="form_content">
+                <div class="auth_card_content">
+                    <span class="auth_name">Авторизация</span>
+                    <hr class="hr_auth" />
+                    <form action="" class="form_auth">
+                        <input placeholder="Введите логин" type="text" class="login">
+                        <input placeholder="Введите пароль" type="password" class="password">
+                    </form>
+                    <div class="btn_content">
+                        <div class="submit_btn">
+                            <button class="submit_auth">
+                                Войти
+                            </button>
+                        </div>
+                        <div class="change_btn">
+                            <a href="#" class="change">Забыли пароль?</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_card">
+                <hr />
+                    <div class="footer_text_content">
+                        <span>
+                            <p>Для того, чтобы получить доступ к этой системе, обратитесь к администратору сайта или директору компании!</p>
+                        </span>
+                        <div class="icons_info">
+                            <div class="icons_info_content">
+                                <img src="../../assets/Auth/man.svg" />
+                                <div>
+                                    <span class="infos_text">Иванов Иван Иванович</span>
+                                </div>
+                            </div>
+                            <div class="icons_info_content">
+                            <img src="../../assets/Auth/phone.svg" />
+                            <div>
+                                <span class="infos_text">+7 (999) 999 99 99</span>
+                            </div>
+                            </div>
+                            <div class="icons_info_content">
+                            <img src="../../assets/Auth/mail.svg" />
+                            <div>
+                                <span class="infos_text">info@gmail.com</span>
+                            </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
