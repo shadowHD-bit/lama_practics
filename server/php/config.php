fa493b6a680180423a62a89438007e4241757a3e
Alexandr <87748202+shadowHD-bit@users.noreply.github.com>
2022-07-06 14:15:54 +0700

    added config

---
+++ server/php/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'UserNameInDatabase' => 'YOUR_NAME',
+    'PasswordUserInDatabase' => 'YOUR_PASSWORD',
+    'HostDatabase' => 'YOUR_HOST',
+    'NameDatabase' => 'YOUR_NAME_DATABASE'
+)
+?>
