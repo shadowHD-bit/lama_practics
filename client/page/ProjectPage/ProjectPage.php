3db7cd2a64aa91cf7adfd998b059660673b9ca1b
Friend34you <vovan23doni@gmail.com>
2022-07-07 11:59:16 +0300

    main_site_structure update

--- client/page/ProjectPage/ProjectPage.php
+++ client/page/ProjectPage/ProjectPage.php
@@ -1,17 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Главная</title>
-    <?php require('../../assets/libraries/head.lib.php')?>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="ProjectPage.css">
+    <title>Главная страница </title>
 </head>
+
 <body>
-hello in project page
 
+<div class="Menu">
+    <div class="profile_wrapper">
+        <button class="Button_profile"></button>
+        <a class="profile_name">Хаблак Владимир Алексеевич</a>
+    </div>
+    <div class="menu_btn_wrapper">
+        <button class="button_menu_text" id="Profile">Профиль</button>
+        <button class="button_menu_text" id="Tasks"> Задачи</button>
+        <button class="button_menu_text" id="Projects">Проекты</button>
 
-<?php require('../../assets/libraries/scripts.lib.php')?>
+        <button class="Exit">
+            <a class="Exit_text"> Выход </a>
+        </button>
+    </div>
+    <div class="Exit_field">
+
+    </div>
+</div>
+
+<div class="main_content">
+    <a id="Task_list"> Список задач </a>
+    <button id="Filt_button">Фильрация</button>
+</div>
 
 </body>
+
 </html>
