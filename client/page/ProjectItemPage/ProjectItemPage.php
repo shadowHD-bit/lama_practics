5f24c5da1e9c2cc12390dbf3056734ce20432ec3
Friend34you <vovan23doni@gmail.com>
2022-07-13 12:28:24 +0300

    placeholders on projectItemPage added

--- client/page/ProjectItemPage/ProjectItemPage.php
+++ client/page/ProjectItemPage/ProjectItemPage.php
@@ -8,89 +8,92 @@
     <title>Проекты</title>
     <link rel="stylesheet" href="./ProjectItemPage.css?v1">
 
-
     <!-- Check Auth script -->
     <script src="../../scripts/ajax/CheckAuth.js"></script>
 </head>
 
 <body>
-    <section class="main_page">
-        <div class="Menu">
-            <div class="profile_wrapper">
-                <div class="Button_profile" id="photo_navs"></div>
-                <!-- BIO user -->
-                <a id="bio_user" class="profile_name"></a>
-            </div>
-            <div class="menu_btn_wrapper">
-                <a href="../ProfilePage/ProfilePage.php" class="button_menu_text" id="Profile">Профиль</a>
-                <a href="../MainPage/MainPage.php" class="button_menu_text" id="Tasks"> Задачи</a>
-                <a href="../ProjectPage/ProjectPage.php" class="button_menu_text" id="Projects">Проекты</a>
-                <button class="Exit" id="logout">
-                    Выход
-                </button>
-            </div>
+<section class="main_page">
+    <div class="Menu">
+        <div class="profile_wrapper">
+            <div class="Button_profile" id="photo_navs"></div>
+            <!-- BIO user -->
+            <a id="bio_user" class="profile_name"></a>
         </div>
-        <div class="main_content">
-            <div class="tasks_page">
+        <div class="menu_btn_wrapper">
+            <a href="../ProfilePage/ProfilePage.php" class="button_menu_text" id="Profile">Профиль</a>
+            <a href="../MainPage/MainPage.php" class="button_menu_text" id="Tasks"> Задачи</a>
+            <a href="../ProjectPage/ProjectPage.php" class="button_menu_text" id="Projects">Проекты</a>
+            <button class="Exit" id="logout">
+                Выход
+            </button>
+        </div>
+    </div>
+    <div class="main_content">
+        <div class="tasks_page">
 
-                <div class="header_content_task">
-                    <a id="task_list">Информация о проекте</a>
+            <div class="header_content_task">
+                <a id="task_list">Информация о проекте</a>
+            </div>
+
+
+            <div class="task_field">
+                <div class="task_name">
+                    <p class="task_title">Название проекта: </p>
+                    <p class="task_info" id="title_project">
+                        <span class="placeholder"></span>
+                    </p>
+                </div>
+                <hr class="hr">
+                <div class="task_deadline">
+                    <p class="task_title">Дата окончания проекта:</p>
+                    <p id="deadline_project" class="task_info">
+                        <span class="placeholder"></span>
+                    </p>
+                </div>
+                <hr class="hr">
+                <div class="task_description">
+                    <p class="task_title">Описание:</p>
+                    <p id="project_description" class="task_info">
+                        <span class="placeholder placeholder_description"></span>
+                    </p>
+                </div>
+                <hr class="hr">
+
+                <div class="task_director">
+                    <p class="task_title">Создатель:</p>
+                    <div id="creatorProject" class="task_member">
+                        <span class="placeholder"></span>
+                    </div>
                 </div>
 
-
-                <div class="task_field">
-
-                    <div class="task_name">
-                        <p class="task_title">Название проекта: </p>
-                        <p class="task_info" id="title_project"></p>
+                <div class="task_performer">
+                    <p class="task_title">Участники:</p>
+                    <div id="ispolnitels" class="players">
+                        <span class="placeholder"></span>
+                        <span class="placeholder"></span>
+                        <span class="placeholder"></span>
+                        <span class="placeholder"></span>
                     </div>
-                    <hr>
-                    <div class="task_deadline">
-                        <p class="task_title">Дата окончания проекта:</p>
-                        <p id="deadline_project" class="task_info"></p>
-                    </div>
-                    <hr>
-                    <div class="task_description">
-                        <p class="task_title">Описание:</p>
-                        <p id="project_description" class="task_info"></p>
-                    </div>
-                    <hr>
-
-                    <div class="task_director">
-                        <p class="task_title">Создатель:</p>
-                        <div id="creatorProject" class="task_member">
-                        </div>
-                    </div>
-
-                    <div class="task_performer">
-                        <p class="task_title">Участники:</p>
-
-                        <div id="ispolnitels" class="players">
-                            
-
-                        </div>
-
-
-
-                    </div>
-
                 </div>
 
-
             </div>
+
         </div>
-    </section>
+
+    </div>
+</section>
 
 
-    <!-- Get task script -->
+<!-- Get task script -->
 
 
-    <!-- Get user script -->
-    <script src="../../scripts/ajax/get/get.data.user.js"></script>
-    <!-- Get project script -->
-    <script src="../../scripts/ajax/get/get.data.this.project.js"></script>
-    <!-- Logout func script -->
-    <script src="../../scripts/ajax/Logout.js"></script>
+<!-- Get user script -->
+<script src="../../scripts/ajax/get/get.data.user.js"></script>
+<!-- Get project script -->
+<script type="module" src="../../scripts/ajax/get/get.data.this.project.js"></script>
+<!-- Logout func script -->
+<script src="../../scripts/ajax/Logout.js"></script>
 
 </body>
 
--- client/scripts/ajax/get/get.data.this.project.js
+++ client/scripts/ajax/get/get.data.this.project.js
@@ -1,7 +1,8 @@
+
 let idProjectFromURL = location.search.substring(1);
 
 let dataThisProject = {
-  dataId: idProjectFromURL,
+    dataId: idProjectFromURL,
 };
 
 let titleProject = document.getElementById("title_project");
@@ -13,77 +14,78 @@ let playersProject = document.getElementById("ispolnitels");
 //Get data project
 
 fetch("../../../../server/php/Project/GetThisProject.php", {
-  method: "POST",
-  body: JSON.stringify(dataThisProject),
-  header: {
-    "Content-Type": "application/json; charset=UTF-8",
-  },
+    method: "POST",
+    body: JSON.stringify(dataThisProject),
+    header: {
+        "Content-Type": "application/json; charset=UTF-8",
+    },
 })
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (body) {
-    body.map((el) => {
-      titleProject.innerHTML = `
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (body) {
+        body.map((el) => {
+            titleProject.innerHTML = `
         ${el.project_name}
         `;
 
-      deadlineProject.innerHTML = `
+            deadlineProject.innerHTML = `
         ${el.project_deadline}
         `;
-      descriptionProject.innerHTML = `
+            descriptionProject.innerHTML = `
         ${el.project_description}
         `;
+        });
     });
-  });
 
 //Get Creator
 
 fetch("../../../../server/php/Project/GetCreatorProject.php", {
-  method: "POST",
-  body: JSON.stringify(dataThisProject),
-  header: {
-    "Content-Type": "application/json; charset=UTF-8",
-  },
+    method: "POST",
+    body: JSON.stringify(dataThisProject),
+    header: {
+        "Content-Type": "application/json; charset=UTF-8",
+    },
 })
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (body) {
-    body.map((el) => {
-      creatorProject.innerHTML = `
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (body) {
+        body.map((el) => {
+            creatorProject.innerHTML = `
         <div style="background-image: url('../../../../server/uploads/${
-          el.photo
-        }')" class="task_member_photo" id="photo_creator"></div>
+                el.photo
+            }')" class="task_member_photo" id="photo_creator"></div>
         <p id="bio_creator" class="task_member_fullname task_info">${
-          el.last_name + " " + el.first_name + " " + el.second_name
-        }</p>
+                el.last_name + " " + el.first_name + " " + el.second_name
+            }</p>
         `;
+        });
     });
-  });
 
 //Get players
 fetch("../../../../server/php/Project/GetPlayersProject.php", {
-  method: "POST",
-  body: JSON.stringify(dataThisProject),
-  header: {
-    "Content-Type": "application/json; charset=UTF-8",
-  },
+    method: "POST",
+    body: JSON.stringify(dataThisProject),
+    header: {
+        "Content-Type": "application/json; charset=UTF-8",
+    },
 })
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (body) {
-    body.map((el) => {
-      playersProject.innerHTML += `
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (body) {
+        playersProject.innerHTML = '';
+        body.map((el) => {
+            playersProject.innerHTML += `
         <div id="ispolnitels" class="task_member">
             <div class="task_member_photo" style="background-image: url('../../../../server/uploads/${
-              el.photo
+                el.photo
             }')"></div>
             <p class="task_member_fullname task_info">${
-              el.last_name + " " + el.first_name + " " + el.second_name
+                el.last_name + " " + el.first_name + " " + el.second_name
             }</p>
         </div>
         `;
+        });
     });
-  });
