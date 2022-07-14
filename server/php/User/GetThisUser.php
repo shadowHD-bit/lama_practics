0b628ba77a7e8bd421845d4dc91aeae2b6216174
Alexandr <87748202+shadowHD-bit@users.noreply.github.com>
2022-07-14 16:03:08 +0700

    save temp result

---
+++ server/php/User/GetThisUser.php
@@ -0,0 +1,9 @@
+<?php
+//Headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: *');
+header('Content-Type: application/json; charset=UTF-8');
+
+//Get id
+echo $_COOKIE['user_id'];
